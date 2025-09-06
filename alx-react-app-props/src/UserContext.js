6aa5ece00136acdde1bf8007e67459946d8fe60c
Ayomide Oluyide <mideoluyide@gmail.com>
2025-09-06 13:56:10 +0100

    Add changes

--- alx-react-app-props/src/UserContext.js
+++ alx-react-app-props/src/UserContext.js
@@ -1,5 +1,5 @@
 // src/UserContext.js
 import { createContext } from 'react';
 
-const UserContext = createContext(null); // default value null is fine here
+const UserContext = createContext(null); // Initializes context with a default value
 export default UserContext;
