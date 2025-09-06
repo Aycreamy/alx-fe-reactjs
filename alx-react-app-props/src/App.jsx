a712207eefbfc6b79c2d9c44b33dab559a8e6e04
Ayomide Oluyide <mideoluyide@gmail.com>
2025-09-06 14:34:11 +0100

    add

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -3,11 +3,9 @@ import ProfilePage from './ProfilePage';
 import UserContext from './UserContext';
 
 function App() {
-  // The user data that we want to share with all components
-  const userData = { name: "Ayomide Oluyide", email: "Aycreamy@gmail.com.com" };
+  const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
 
   return (
-    // Wrap the entire app (or the part that needs the data) in the Provider
     <UserContext.Provider value={userData}>
       <ProfilePage />
     </UserContext.Provider>
