3c739af73b8d29356fa43cfa0b9a39e95c4b09c3
Ayomide Oluyide <mideoluyide@gmail.com>
2025-09-07 09:38:35 +0100

    Router Abeg added

--- my-company/App.jsx
+++ my-company/App.jsx
@@ -1,10 +1,11 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './Navbar';
-import Home from './Home';
-import About from './About';
-import Services from './Services';
-import Contact from './Contact';
+// src/App.jsx
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./Navbar";
+import Home from "./Home";
+import About from "./About";
+import Services from "./Services";
+import Contact from "./Contact";
 
 function App() {
   return (
