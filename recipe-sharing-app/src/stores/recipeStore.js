804511202eb1b8f1b142a5c75111c5ae2a1c7438
Ayomide Oluyide <mideoluyide@gmail.com>
2025-09-13 14:27:14 +0100

    Add

--- recipe-sharing-app/src/stores/recipeStore.js
+++ recipe-sharing-app/src/stores/recipeStore.js
@@ -1,15 +1,11 @@
-// src/stores/recipeStore.js
+// src/components/recipeStore.js
 import create from 'zustand';
 
-/**
- * useRecipeStore - Zustand store for recipes
- * - recipes: array of { id, title, description }
- * - addRecipe(newRecipe): append a recipe
- * - setRecipes(recipes): replace the list (useful for init)
- */
 export const useRecipeStore = create((set) => ({
   recipes: [],
   addRecipe: (newRecipe) =>
-    set((state) => ({ recipes: [...state.recipes, newRecipe] })),
+    set((state) => ({
+      recipes: [...state.recipes, newRecipe],
+    })),
   setRecipes: (recipes) => set({ recipes }),
 }));
